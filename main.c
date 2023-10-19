4e02f9467f2be1d958972d3ee6a59344fbeb3ffe
justonlyme1 <taiwomiracleveecthor@gmail.com>
2023-10-18 22:01:44 -0700

    shell prototype

--- main.c
+++ main.c
@@ -1,36 +1,74 @@
-#include "shell.h"
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+
+#include "main.h"
 
 /**
- * main - Entry point for the simple shell.
+ * free_data - frees data structure
  *
- * Return: Always 0.
+ * @datash: data structure
+ * Return: no return
  */
-int main(void)
+void free_data(data_shell *datash)
 {
-	char input[1024];
+	unsigned int i;
 
-	while (1)
+	for (i = 0; datash->_environ[i]; i++)
 	{
-		display_prompt();
-
-		if (fgets(input, sizeof(input), stdin) == NULL)
-		{
-			printf("\n");
-			break;
-		}
-
-		input[strlen(input) - 1] = '\0';
-
-		if (strlen(input) == 0)
-		{
-			continue;
-		}
-
-		execute_command(input);
+		free(datash->_environ[i]);
 	}
 
-	return (0);
+	free(datash->_environ);
+	free(datash->pid);
+}
+
+/**
+ * set_data - Initialize data structure
+ *
+ * @datash: data structure
+ * @av: argument vector
+ * Return: no return
+ */
+void set_data(data_shell *datash, char **av)
+{
+	unsigned int i;
+
+	datash->av = av;
+	datash->input = NULL;
+	datash->args = NULL;
+	datash->status = 0;
+	datash->counter = 1;
+
+	for (i = 0; environ[i]; i++)
+		;
+
+	datash->_environ = malloc(sizeof(char *) * (i + 1));
+
+	for (i = 0; environ[i]; i++)
+	{
+		datash->_environ[i] = _strdup(environ[i]);
+	}
+
+	datash->_environ[i] = NULL;
+	datash->pid = aux_itoa(getpid());
+}
+
+/**
+ * main - Entry point
+ *
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return: 0 on success.
+ */
+int main(int ac, char **av)
+{
+	data_shell datash;
+	(void) ac;
+
+	signal(SIGINT, get_sigint);
+	set_data(&datash, av);
+	shell_loop(&datash);
+	free_data(&datash);
+	if (datash.status < 0)
+		return (255);
+	return (datash.status);
 }
